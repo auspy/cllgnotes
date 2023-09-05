ee0a07a9bc3bbfb6c03ad5f96183d5228dabb31c
auspy <vinayak.kshetez0102@gmail.com>
2023-09-06 02:31:55 +0530

    main: feat: shared css from ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page(): JSX.Element {
   log("Hello World");
   return (
     <>
-      <div className="p-[100px] text-zinc-500 ps-50 ml-50 mb-50 m-100 test bg-blue-500">
+      <div className="p-[100px] test text-zinc-500 ps-50 ml-50 mb-50 m-100 test bg-blue-500">
         hello it is
       </div>
       <Header text="Web" />
