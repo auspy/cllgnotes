0f7eaa38b8f7c47f9f63316c7e76b4bfceccc719
auspy <vinayak.kshetez0102@gmail.com>
2023-09-06 03:05:08 +0530

    main: feat: setup lib

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,9 @@
 import { log, logger } from "logger";
 import { Button, Header, Logo } from "ui";
+import { CONSTANT_TEST } from "@cllgnotes/lib/constants";
 
 export default function Page(): JSX.Element {
-  log("Hello World");
+  log("Hello World" + CONSTANT_TEST);
   return (
     <>
       <div className="p-[100px] test text-zinc-500 ps-50 ml-50 mb-50 m-100 test bg-blue-500">
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["ui", "types"],
+  transpilePackages: ["ui", "types", "@cllgnotes/lib"],
 };
