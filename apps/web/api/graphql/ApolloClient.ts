4285e08952b7148173e69fd1d35f99abd4b400d4
auspy <vinayak.kshetez0102@gmail.com>
2024-03-12 04:41:36 +0530

    fix: remove createUploadLink for now

--- apps/web/api/graphql/ApolloClient.ts
+++ apps/web/api/graphql/ApolloClient.ts
@@ -1,24 +1,24 @@
 import { urlGql } from "@/constants";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 import { cacheOptions } from "./cache.config";
-import { createUploadLink } from "apollo-upload-client";
+// import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(cacheOptions),
-    link: createUploadLink({
+    link: new HttpLink({
+      // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: urlGql,
       credentials: "include",
+      // you can disable result caching here if you want to
+      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       fetchOptions: { cache: "no-store" },
     }),
-    // new HttpLink({
-    //   // this needs to be an absolute url, as relative urls cannot be used in SSR
+    // createUploadLink({
     //   uri: urlGql,
     //   credentials: "include",
-    //   // you can disable result caching here if you want to
-    //   // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     //   fetchOptions: { cache: "no-store" },
     // }),
   });
