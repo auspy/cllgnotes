2275dc84ba3da46fa82a27d8e5a5ee20cb3dc04f
auspy <vinayak.kshetez0102@gmail.com>
2024-03-12 08:28:39 +0530

    feat: added ratelimit

--- apps/web/constants.ts
+++ apps/web/constants.ts
@@ -1,4 +1,4 @@
 const port = 3002;
-export const isDev = true;
+export const isDev = process.env.NODE_ENV === "development";
 export const urlGql =
   (isDev ? process.env.NEXT_PUBLIC_API_URL : "") + "/graphql";
--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,12 +1,49 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis as Rd } from "@upstash/redis";
+import { isDev } from "./constants";
 
 // middleware is applied to all routes, use conditionals to select
-
+const cache = new Map();
+const ratelimit =
+  !isDev &&
+  new Ratelimit({
+    redis: Rd.fromEnv(),
+    timeout: 1000,
+    analytics: true,
+    ephemeralCache: cache,
+    limiter: Ratelimit.slidingWindow(80, "60 s"),
+  });
 export default withAuth(
-  function middleware(req) {
-    console.log("Incoming request:", req.method, req.url);
-    return NextResponse.next();
+  async function middleware(req) {
+    // return NextResponse.next();
+    console.log("Incoming request:", req.method, req.url, req.ip);
+    if (isDev) {
+      console.log("dev mode");
+      return NextResponse.next();
+    }
+    if (!ratelimit) {
+      console.error("middleware: ratelimit not initialized");
+      return NextResponse.next();
+    }
+    const ip = req.ip ?? "127.0.0.1";
+    const { success, pending, limit, reset, remaining } =
+      await ratelimit.limit(ip);
+    console.log(
+      "ratelimit: within limit?",
+      success,
+      await pending,
+      limit,
+      reset,
+      remaining
+    );
+    return success
+      ? NextResponse.next()
+      : NextResponse.json(
+          { error: "rate limit exceeded" },
+          { status: 429, statusText: "Rate imit exceeded" }
+        );
   },
   {
     callbacks: {
