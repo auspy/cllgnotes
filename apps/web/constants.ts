0133431e5bb51239ee636b0e58a3f0343cc4e88a
auspy <vinayak.kshetez0102@gmail.com>
2024-03-13 17:35:28 +0530

    feat: change url for prod

--- apps/web/constants.ts
+++ apps/web/constants.ts
@@ -1,4 +1,5 @@
 const port = 3002;
 export const isDev = process.env.NODE_ENV === "development";
 export const urlGql =
-  (isDev ? process.env.NEXT_PUBLIC_API_URL : "") + "/graphql";
+  (process.env.NEXT_PUBLIC_API_URL || "http://localhost:3002") + "/graphql";
+console.log(urlGql);
