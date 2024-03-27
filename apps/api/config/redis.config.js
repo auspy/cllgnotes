d9f8e1e6cd39a15f4281ece45efa33a9adac1bcb
auspy <vinayak.kshetez0102@gmail.com>
2024-03-27 15:11:34 +0530

    fix: redis config

--- apps/api/config/redis.config.js
+++ apps/api/config/redis.config.js
@@ -4,7 +4,10 @@ import { env } from "node:process";
 import { Redis } from "ioredis";
 // import { Redis } from "@upstash/redis";
 console.log("process.env.ENV", process.env.TEST);
-const redisClient = new Redis();
+const redisClient = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 // process.env.NODE_ENV == "development" || env.NODE_ENV == "development"
 //   ? "redis://localhost:6379"
